e12d5bf1bc71cf3e1342189c0820e5e6ae0c3892
jitasek <jitka.moreira@gmail.com>
2021-10-26 23:43:50 +0200

    get weather data

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,3 +19,54 @@ const getCurrentData = function (name, forecastData) {
 const getFormattedDate = function (unixTimestamp, format = "DD/MM/YYYY") {
   return moment.unix(unixTimestamp).format(format);
 };
+
+const getForecastData = function (forecastData) {
+  const callback = function (each) {
+    return {
+      date: getFormattedDate(each.dt),
+      temperature: each.temp.max,
+      wind: each.wind_speed,
+      humidity: each.humidity,
+      iconCode: each.weather[0].icon,
+    };
+  };
+
+  return forecastData.daily.slice(1, 6).map(callback);
+};
+
+const getWeatherData = async (cityName) => {
+  const currentDataUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
+  const currentDataResponse = await fetch(currentDataUrl);
+  const currentData = await currentDataResponse.json();
+
+  const lat = currentData.coord.lat;
+  const lon = currentData.coord.lon;
+  const name = currentData.name;
+
+  const forecastDataUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`;
+
+  const forecastDataResponse = await fetch(forecastDataUrl);
+  const forecastData = await forecastDataResponse.json();
+
+  const current = getCurrentData(name, forecastData);
+  const forecast = getForecastData(forecastData);
+
+  return {
+    current: current,
+    forecast: forecast,
+  };
+};
+
+const setCitiesInLS = function (cityName) {
+  // get cities from local storage
+  const cities = JSON.parse(localStorage.getItem("recentCities")) ?? [];
+
+  // if city does not exist
+  if (!cities.includes(cityName)) {
+    // insert cityName in cities
+    cities.push(cityName);
+
+    // set cities in local storage
+    localStorage.setItem("recentCities", JSON.stringify(cities));
+  }
+};
